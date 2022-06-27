e50f0320c6da0a90af548c66d957cdeb422c8f74
Rishabh27 <37605701+RishabhSelenium@users.noreply.github.com>
2022-06-28 03:15:06 +0530

    Update JsonVariable.java

--- src/test/java/org/testing/Utility/JsonVariable.java
+++ src/test/java/org/testing/Utility/JsonVariable.java
@@ -5,8 +5,8 @@ import java.util.regex.Pattern;
 public class JsonVariable {
 
 	//Mehthod to replace the value
-	public static String jsonVariableReplacement(String data, String keyName, String keyValue) {
-		
+	public static String jsonVariableReplacement(String data, String keyName, String keyValue)
+	{
 
 		data = data.replaceAll(Pattern.quote("{{"+keyName+"}}"), keyValue);
 		return data;
